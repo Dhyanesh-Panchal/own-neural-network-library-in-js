611b75880a0410964f15fb55ec6cc228cbf4574a
Dhyanesh Panchal <dhynincoding2004@gmail.com>
2023-08-12 22:53:24 +0530

    Initial setup

---
+++ neural_network.js
@@ -0,0 +1,7 @@
+class NeuralNetwork {
+    constructor(no_of_input_node, no_of_hidden_nodes, no_of_output_node) {
+        this.input_nodes = no_of_input_node;
+        this.output_nodes = no_of_output_node;
+        this.hidden_nodes = no_of_hidden_nodes;
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,12 @@
+let mynn;
+
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	mynn = new NeuralNetwork(2, 3, 2);
+
+}
+
+function draw() {
+	// put drawing code here
+}
